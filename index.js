5a4e9bba5c7d7b324786f80f1ce263b0b797d521
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-03-25 14:16:18 +0300

    completes test

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // code your solution here
 function superbowlWin(record) {
-    const win = record.find((game) => game.result === "W");
+    const win = record.find((record) => record.result === "W");
     if (win) { return win.year; } else { return undefined; }
 }
 
 // Example of usage of .find
-const games = [
+const record = [
 { year: 2015, result: "W" },
 { year: 2014, result: "N/A" },
 { year: 2013, result: "L" },
 ];
 
-const broncosWinYear = superbowlWin(games);
+const broncosWinYear = superbowlWin(record);
 console.log(broncosWinYear);
